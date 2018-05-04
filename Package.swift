d7b9f6a16e31616659cb15013ac6d8c10f1d04d8
Tiago Ferreira <me@tiferrei.com>
2018-05-04 20:32:37 +0100

    RequestKit Swift PM support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "UCLKit", targets: ["UCLKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/nerdishbynature/RequestKit.git", from: "2.2.0")
+        .package(url: "https://github.com/nerdishbynature/RequestKit.git", .branch("swiftPackageManager"))
     ],
     targets: [
         .target(name: "UCLKit", dependencies: ["RequestKit"], path: "Source"),
