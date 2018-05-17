e84ae0fe7fec3251b596acc93b225b5768c27b18
Tiago Ferreira <me@tiferrei.com>
2018-05-17 17:02:22 +0100

    Index Linux tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,4 +1,12 @@
 import XCTest
 @testable import UCLKitTests
 
-XCTMain(UCLKitTests.allTests())
+XCTMain([
+    testCase(UCLKitTests.allTests),
+    testCase(RoomTests.allTests),
+    testCase(BookingTests.allTests),
+    testCase(EquipmentTests.allTests),
+    testCase(FreeRoomsTests.allTests),
+    testCase(SearchTests.allTests),
+    testCase(HelperToolsTests.allTests)
+])
